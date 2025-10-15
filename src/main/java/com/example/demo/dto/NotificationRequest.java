70f02bedfe6c834301a0b077fdbf41bbe2509046
James Huang <James781004@hotmail.com>
2025-10-15 15:05:13 +0800

    fix test cases

--- src/main/java/com/example/demo/dto/NotificationRequest.java
+++ src/main/java/com/example/demo/dto/NotificationRequest.java
@@ -1,12 +1,20 @@
 package com.example.demo.dto;
 
 import com.example.demo.model.NotificationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class NotificationRequest {
     private NotificationType type;
     private String recipient;
     private String subject;
     private String content;
+
+
 }
